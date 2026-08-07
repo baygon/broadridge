b44799cb578b1d0acfc8241fc7fe5cdae9515101
janistrucelj <jankostrucelj@hotmail.com>
2026-08-07 12:11:26 +0200

    feat: improved count-format pipe to support M and B numbers
    (in requirement its stated that we should support only K numbers, but it bothered me too much visually)

--- package-viewer/src/app/pipes/count-format.pipe.ts
+++ package-viewer/src/app/pipes/count-format.pipe.ts
@@ -1,11 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const UNITS = [
+  { threshold: 1_000_000_000, suffix: 'B' },
+  { threshold: 1_000_000, suffix: 'M' },
+  { threshold: 1_000, suffix: 'K' },
+];
+
 @Pipe({
   name: 'countFormat',
   standalone: true,
 })
 export class CountFormatPipe implements PipeTransform {
   transform(value: number): string {
-    return value > 1000 ? `${Math.floor(value / 1000)}K` : `${value}`;
+    const unit = UNITS.find(({ threshold }) => value > threshold);
+    return unit
+      ? `${Math.floor(value / unit.threshold)}${unit.suffix}`
+      : `${value}`;
   }
 }
